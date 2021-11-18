ba9d321cb9580da65148f2e79ffec05a14ff324a
GamuchiraiS <80484981+GamuchiraiS@users.noreply.github.com>
2021-11-18 19:46:32 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace P1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Str w = new Str(); //create an object for the Str class 
+            w.ReverseWord(); //call the method
+            Console.WriteLine(w.str); //prints the reversed string
+            Console.ReadLine();
+
+        }
+        // create class 
+        public class Str
+        {
+            public string str; //create a field
+            //constructor
+            public Str()
+            {
+                str = "Hello to the world"; //assign a value
+            }
+            //method
+            public void ReverseWord()
+            {
+                //Creates an array filled with the words of the string
+                string [] wordArray = str.Split(' '); 
+                //Reverses the array
+                Array.Reverse(wordArray);
+                //Joins the reversed array and assigns the value
+                //to str
+                str = String.Join(" ", wordArray);
+            }
+        }
+    }
+}
